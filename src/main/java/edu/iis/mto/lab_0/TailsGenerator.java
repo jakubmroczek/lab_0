7cf75a4080e81514ee17bc2413ca85b5e66c10c4
Jakub Mroczek <jakub.mroczek@tomtom.com>
2019-03-07 18:04:48 +0100

    implemented tails method

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,28 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        if (isValid(value)) {
+            return generateTails(value);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    private boolean isValid(String value) {
+        return value != null;
+    }
+
+    private List<String> generateTails(String value) {
+        List<String> tailsList = new ArrayList<>();
+
+        for (int i = 0; i < value.length(); i++) {
+            String tail = value.substring(i);
+            tailsList.add(tail);
+        }
+
+        tailsList.add("");
+
+        return tailsList;
     }
 
 }
